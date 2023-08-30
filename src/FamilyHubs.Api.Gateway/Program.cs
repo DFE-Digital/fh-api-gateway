4e38faf197e4e69652443d957488dcb472521a9d
Martin-Belton-gov <martin.belton@digital.education.gov.uk>
2023-08-30 11:19:11 +0100

    Initial Configuration

---
+++ src/FamilyHubs.Api.Gateway/Program.cs
@@ -0,0 +1,18 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+// ref: https://medium.com/aspnetrun/building-ocelot-api-gateway-microservices-with-asp-net-core-and-docker-container-13f96026e86c
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)  //Directory.GetCurrentDirectory());
+        .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
+        .AddEnvironmentVariables();
+
+builder.Services.AddOcelot(builder.Configuration);
+
+var app = builder.Build();
+await app.UseOcelot();
+
+//app.MapGet("/", () => "Hello World!");
+
+app.Run();
